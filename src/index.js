b72d88c38276d8254aa8fa7f1ae7d2b9d3390500
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2021-01-22 12:05:33 -0300

    Finished the first concepts of NodeJS.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ const express  = require('express');
 
 const app = express();
 
+/*
+    kinds of params.
+    query parms = ?name=value
+    route parms = /:id
+    Reques Body = Coming from JSON
+*/
+
+app.use(express.json());
+
 app.get('/projects', (request, response) =>{
+    const query = request.query;
+    console.log(query);
+
     return response.json([
         'Project 1',
         'Project 2'
@@ -11,14 +23,21 @@ app.get('/projects', (request, response) =>{
 });
 
 app.post('/projects', (request, response) => {
-    return response.json({message: "Data criated."});
+    const {title, name} = request.body;
+        console.log(title, name)
 });
 
 app.put('/projects/:id', (request, response) => {
+    const params = request.params;
+    console.log(params, requestbody);
+
     return response.json({message: "Data refresed."});
 });
 
 app.delete('/projects/:id', (request, response) => {
+    const params = request.params;
+    console.log(params);
+
     return response.json({message: "Data deleted."})
 })
 
