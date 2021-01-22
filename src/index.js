a7dadcfad3ba17bba3aa262d36bed9b05184941c
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2021-01-22 10:46:45 -0300

    Added the 4 main methods of requests and testing all this requests with Insomnia. Too it is add Nodemon to continuos refresh.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 const express  = require('express');
 
 const app = express();
+
 app.get('/projects', (request, response) =>{
-    return response.json({
-        message: "Hello World!"
-    });
+    return response.json([
+        'Project 1',
+        'Project 2'
+    ]);
     //Always will return a JSON response.
+});
+
+app.post('/projects', (request, response) => {
+    return response.json({message: "Data criated."});
+});
+
+app.put('/projects/:id', (request, response) => {
+    return response.json({message: "Data refresed."});
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json({message: "Data deleted."})
 })
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('The server is running 🚀');
+});
